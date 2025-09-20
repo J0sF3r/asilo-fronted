4bbcb584af8f340fa9dff8b9a5c25301d8b27b4c
J0sFer <jdiazm17@gmail.com>
2025-09-20 02:17:47 -0600

    cambio de url

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://asilo-backend-api.onrender.com',
+    baseURL: 'https://asilo-backend-api.onrender.com/api' ,
     headers: {
         'Content-Type': 'application/json'
     }
