4b6aa0802da8da6c88d2022cecf2d983f31a13b6
J0sF3r <jdiazm17@miumg.edu.gt>
2025-09-20 02:54:50 -0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://asilo-backend-api.onrender.com/api' ,
+    baseURL: 'https://asilo-backend-api.onrender.com' ,
     headers: {
         'Content-Type': 'application/json'
     }
