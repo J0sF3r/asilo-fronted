4731797cbad3654a493fecef811daa52587664d6
J0sF3r <jdiazm17@miumg.edu.gt>
2025-09-29 00:34:30 -0600

    Regresar a Home

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import { Link as RouterLink } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 const MainLayout = ({ children }) => {
@@ -19,6 +22,17 @@ const MainLayout = ({ children }) => {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Asilo de Ancianos Cabeza de Algodon
                     </Typography>
+
+                     <IconButton
+                    color="inherit"
+                    component={RouterLink}
+                    to="/"
+                    aria-label="ir al dashboard"
+                    title="Ir al Dashboard" // Texto que aparece al pasar el mouse
+                >
+                    <DashboardIcon />
+                </IconButton>
+                
                     <Button color="inherit" endIcon={<LogoutIcon />}  onClick={handleLogout}>Cerrar Sesión</Button>
                 </Toolbar>
             </AppBar>
