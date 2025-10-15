253a5792c53bd123169f1ea20c710972052a3f9d
J0sF3r <jdiazm17@miumg.edu.gt>
2025-10-14 19:56:28 -0600

    Empezando modulo de Reportes

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -37,6 +37,34 @@ const DashboardPage = () => {
             <Typography variant="subtitle1" color="text.secondary" sx={{ mb: 4 }}>
                 Facilitando tu labor con cada módulo, Tu espacio de cuidado, a un clic
             </Typography>
+
+            {/* Este componente solo se muestra si el rol es Admin o Médico General */}
+            {(userRole === 'Medico Especialista') && <PendientesRevision />}
+
+            <Grid container spacing={4}>
+                {dashboardItems.map((item) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={item.text}>
+                        <ButtonBase 
+                            component={Link} 
+                            to={item.path}
+                            sx={{ 
+                                width: '100%', 
+                                height: '100%', 
+                                borderRadius: 2, 
+                                '&:hover': { boxShadow: 6, transform: 'translateY(-4px)' },
+                                transition: 'all 0.2s ease-in-out'
+                            }}
+                        >
+                            <Paper elevation={3} sx={{ p: 3, textAlign: 'center', width: '100%', minHeight: 180, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', borderRadius: 2 }}>
+                                {React.cloneElement(item.icon, { sx: { fontSize: 50 }, color: "primary" })}
+                                <Typography variant="h6" component="h2" sx={{ mt: 2, fontWeight: 500 }}>
+                                    {item.text}
+                                </Typography>
+                            </Paper>
+                        </ButtonBase>
+                    </Grid>
+                ))}
+            </Grid>
         </Box>
     );
 };
