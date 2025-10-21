24a238eafe0fa4666f7b3e5a1763d240afbac773
J0sF3r <jdiazm17@miumg.edu.gt>
2025-10-20 22:21:36 -0600

    mejorarRutas

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,5 +1,5 @@
 // src/components/ProtectedRoute.js
-import React from 'react';
+/*import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
@@ -12,6 +12,36 @@ const ProtectedRoute = ({ children }) => {
 
     // Si hay un token, muestra el contenido que protege
     return children;
+};
+
+export default ProtectedRoute;*/
+
+// src/components/ProtectedRoute.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  try {
+    const decoded = jwtDecode(token);
+    const now = Date.now() / 1000; // tiempo actual en segundos
+
+    if (decoded.exp < now) {
+      localStorage.removeItem('token');
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  } catch (error) {
+    localStorage.removeItem('token');
+    return <Navigate to="/login" />;
+  }
 };
 
 export default ProtectedRoute;
